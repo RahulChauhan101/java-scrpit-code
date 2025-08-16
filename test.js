af33c7cb8d4bc25fd0a50a6d80abfd90b13f1a7a
RahulChauhan101 <rahul.chauhan12241@gmail.com>
2025-08-16 20:36:37 +0530

    16/8/2025

--- test.js
+++ test.js
@@ -86,43 +86,66 @@
 // console.log(`Key:`, findkeyDepth(data, 'd'));
 // console.log("Key:" ,targetkey,"depth:", findkeyDepth(data, targetkey)); 
 
-const Data = {
-  a: 1,
-  b: { x: 2, y: { z: 1 } },
-  c: 1
+// const Data = {
+//   a: 1,
+//   b: { x: 2, y: { z: 1 } },
+//   c: 1
+// };
+
+// let valueToFind = 1;
+// let valueToFind2 = 2;
+// function findValues(obj, Value) {
+//   const result = [];
+
+//   function search(object) {
+//     for (const key in object) {
+//       if (typeof object[key] === "object" && object[key] !== null) {
+//         search(object[key]);
+//       }
+//       else {
+//         if (object[key] === Value) {
+//           result.push(key)
+//         }
+//       }
+//     }
+//   }
+//   search(obj);
+//   return result;
+// }
+
+// console.log(findValues(Data, 1));
+// console.log(`Data key  of value ${valueToFind}  = `, findValues(Data, 1));
+// console.log(`Data key of value ${valueToFind}  = ${findValues(Data, 1)}`);
+
+
+// console.log(findValues(Data, 2));
+// console.log(`Data Key of value ${valueToFind2}  = ${findValues(Data, 2)}`);
+// console.log(`Data key of value null  =`, findValues(Data, 3));
+
+
+
+const input = {
+  a: { b:{ c: 5 } },
+  d: 10
 };
 
-let valueToFind = 1;
-let valueToFind2 = 2;
-function findValues(obj, Value) {
-  const result = [];
+function flattenobject(obj, perentkey = "", result = {}) {
+  for (const key in obj) {
+    const newKey = perentkey ? `${perentkey}.${key}` : key;
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      console.log(`qwe Flattening Key: ${newKey}`);
 
-  function search(object) {
-    for (const key in object) {
-      if (typeof object[key] === "object" && object[key] !== null) {
-        search(object[key]);
-      }
-      else {
-        if (object[key] === Value) {
-          result.push(key)
-        }
-      }
+      flattenobject(obj[key], newKey, result);
+      console.log(`Flattened Key: ${newKey}`);
+    } else {
+      result[newKey] = obj[key];
+      console.log(`asd  Flattened Key: ${newKey}, Value: ${obj[key]}`);
     }
   }
-  search(obj);
   return result;
+
 }
+const output = flattenobject(input);
 
-console.log(findValues(Data, 1)); 
-console.log(`Data key  of value ${valueToFind}  = `,findValues(Data,1)); 
-console.log(`Data key of value ${valueToFind}  = ${findValues(Data, 1)}`);
-
-
-console.log(findValues(Data, 2)); 
-console.log(`Data Key of value ${valueToFind2}  = ${findValues(Data, 2)}`);
-console.log(`Data key of value null  =`, findValues(Data, 3));
-
-
-
-
-
+// console.log("Input Object:", input);
+console.log("Flattened Object:", output);
