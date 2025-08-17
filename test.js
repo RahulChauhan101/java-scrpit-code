da862fa16d56da88014d6f21cd05513dc7f22004
RahulChauhan101 <rahul.chauhan12241@gmail.com>
2025-08-17 12:22:08 +0530

    17/8/2025

--- test.js
+++ test.js
@@ -124,94 +124,98 @@
 
 
 
-// const input = {
-//   a: { b:{ c: 5 } },
-//   d: 10
-// };
+const input = {
+  a: { b:{ c: 5 } },
+  d: 10,
+  // e: { f: { g: 15, h: 20 }, 
+  // i: 25 },
+};
 
-// function flattenobject(obj, perentkey = "", result = {}) {
-//   for (const key in obj) {
-//     const newKey = perentkey ? `${perentkey}.${key}` : key;
-//     if (typeof obj[key] === 'object' && obj[key] !== null) {
-//       console.log(`qwe Flattening Key: ${newKey}`);
+function flattenobject(obj, perentkey = "", result = {}) {
+  for (const key in obj) {
+    const newKey = perentkey ? `${perentkey}.${key}` : key;
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      console.log(`asd Flattening Key: ${newKey}`);
 
-//       flattenobject(obj[key], newKey, result);
-//       console.log(`Flattened Key: ${newKey}`);
-//     } else {
-//       result[newKey] = obj[key];
-//       console.log(`asd  Flattened Key: ${newKey}, Value: ${obj[key]}`);
-//     }
-//   }
-//   return result;
-
-// }
-// const output = flattenobject(input);
-
-// // console.log("Input Object:", input);
-// console.log("Flattened Object:", output);
-
-
-let matrix = [
-  [1,2,3],
-  [4,5,6],
-  [7,8,9],
-];
-
-// let matrix = [
-//   [1,2,3,10,11,12],
-//   [4,5,6,13,14,15],
-//   [7,8,9,19,17,18],
-// ];
-
-function spiralOrder(matrix) {
-  let result = [];
-  if (matrix.length === 0) return result;
-
-  let top = 0, 
-      bottom = matrix.length - 1;
-  let left = 0, 
-      right = matrix[0].length - 1;
-
-  while (top <= bottom && left <= right) {
-    // Traverse from Left → Right
-    for (let i = left; i <= right; i++) {
-      result.push(matrix[top][i]);
-      console.log(`Traversing [${top}][${i}]: ${matrix[top][i]}`);
-    }
-    top++;
-    console.log(`Top boundary moved to: ${top}`);
-
-    // Traverse from Top → Bottom
-    for (let i = top; i <= bottom; i++) {
-      result.push(matrix[i][right]);
-      console.log(`Traverse from Top → Bottom [${i}][${right}]: ${matrix[i][right]}`);
-    }
-    right--;
-    console.log(`Right boundary moved to: ${right}`);
-
-    // Traverse from Right → Left
-    if (top <= bottom) {
-      for (let i = right; i >= left; i--) {
-        result.push(matrix[bottom][i]);
-        console.log(`Traverse from Right → Left [${bottom}][${i}]: ${matrix[bottom][i]}`);
-      }
-      bottom--;
-    }
-    console.log(`Bottom boundary moved to: ${bottom}`);
-
-    // Traverse from Bottom → Top
-    if (left <= right) {
-      for (let i = bottom; i >= top; i--) {
-        result.push(matrix[i][left]);
-      }
-      left++;
-      console.log(`Left boundary moved to: ${left}`);
+      flattenobject(obj[key], newKey, result);
+      console.log(`asdasd Flattened Key: ${newKey}`);
+    } else {
+      result[newKey] = obj[key];
+      console.log(`wqewqe  Flattened Key: ${newKey}, Value: ${obj[key]}`);
     }
   }
-
   return result;
-}
-console.log("Spiral Order:", spiralOrder(matrix).join(","));
 
-let output = matrix.flat().join(",");
-console.log("Flattened Output:", output);
+}
+const output = flattenobject(input);
+
+// console.log("Input Object:", input);
+console.log("Flattened Object:", output);
+
+
+// let matrix = [
+//   [1,2,3],
+//   [4,5,6],
+//   [7,8,9],
+// ];
+
+// let matrix = [
+//   [1, 2, 3, 4, 5],
+//   [6, 7, 8, 9, 10],
+//   [11, 12, 13, 14, 15],
+//   [16, 17, 18, 19, 20]
+// ];
+
+// function spiralOrder(matrix) {
+//   let result = [];
+//   if (matrix.length === 0) return result;
+
+//   let top = 0,
+//     bottom = matrix.length - 1;
+//   let left = 0,
+//     right = matrix[0].length - 1;
+
+//   while (top <= bottom && left <= right) {
+//     // Traverse from Left → Right
+//     for (let i = left; i <= right; i++) {
+//       result.push(matrix[top][i]);
+//       console.log(`Traversing from Left → Right [${top}][${i}]: ${matrix[top][i]}`);
+//     }
+//     top++;
+//     console.log(`Top boundary moved to: ${top}`);
+
+//     // Traverse from Top → Bottom
+//     for (let i = top; i <= bottom; i++) {
+//       result.push(matrix[i][right]);
+//       console.log(`Traverse from Top → Bottom [${i}][${right}]: ${matrix[i][right]}`);
+//     }
+//     right--;
+//     console.log(`Right boundary moved to: ${right}`);
+
+//     // Traverse from Right → Left
+//     if (top <= bottom) {
+//       for (let i = right; i >= left; i--) {
+//         result.push(matrix[bottom][i]);
+//         console.log(`Traverse from Right → Left [${bottom}][${i}]: ${matrix[bottom][i]}`);
+//       }
+//       bottom--;
+//     }
+//     console.log(`Bottom boundary moved to: ${bottom}`);
+
+//     // Traverse from Bottom → Top
+//     if (left <= right) {
+//       for (let i = bottom; i >= top; i--) {
+//         result.push(matrix[i][left]);
+//         console.log(`Traverse from Bottom → Top [${i}][${left}]: ${matrix[i][left]}`);
+//       }
+//       left++;
+//       console.log(`Left boundary moved to: ${left}`);
+//     }
+//   }
+
+//   return result;
+// }
+// console.log("Spiral Order:", spiralOrder(matrix).join(","));
+
+// let output = matrix.flat().join(",");
+// console.log("Flattened Output:", output);
